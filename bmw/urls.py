a06dd5f9ffc925488f8ef93a4d6cf1768a993ab0
farad213 <farad213@gmail.com>
2022-11-26 11:55:57 +0100

    redesigned home page

--- bmw/urls.py
+++ bmw/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('buildings', views.buildings, name="buildings"),
-    path('jelkod', views.jelkod, name="length_calc"),
+    path('jelkod', views.jelkod, name="jelkod"),
 ]
