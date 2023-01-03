aff6bc77c4f1431414e5fd8d0716277949aba398
farad213 <farad213@gmail.com>
2023-01-04 00:30:46 +0100

    not relevant

--- schedule/admin.py
+++ schedule/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Vehicle, Project, Date, DateBoundWithProject, Profile, Artifact, Subproject
 
 
-# admin.site.register(Employee)
 admin.site.register(Vehicle)
 admin.site.register(Project)
 admin.site.register(Date)
