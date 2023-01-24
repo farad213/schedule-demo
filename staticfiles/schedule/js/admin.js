9d6a8e06a53032fb39074f43e9cbf95541b2f7bc
farad213 <farad213@gmail.com>
2023-01-24 18:17:11 +0100

    collectstatic and debug false

--- staticfiles/schedule/js/admin.js
+++ staticfiles/schedule/js/admin.js
@@ -1,4 +1,5 @@
-$('.icons > div').hover(function() {
+$( document ).ready(function() {
+    $('.icons > div').hover(function() {
   if (this.classList.contains('true')) {
     // Get the value of the data-bubble attribute and parse it into a JavaScript array
     console.log($(this).data('bubble'))
@@ -18,4 +19,29 @@ $('.icons > div').hover(function() {
 }, function() {
   // Remove the bubble when the mouse leaves the icon
   $(this).find('.bubble').remove();
+
+});
+  var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+
+today = mm + '/' + dd;
+
+$('.day .date').each(function() {
+    console.log(today, $(this).text())
+    if ($(this).text() === today) {
+        $(this).closest('.day').attr('id', 'today');
+    }
+});
+
+ $(".toggle-days-displayed input[name='toggle-days-displayed']").click(function() {
+        if ($(this).val() === "5 nap") {
+            $(".weekend, .Szombat-display, .Vasárnap-display").css("display", "none");
+            $(".day_wrap, .weekdays div").css("margin", "10px 20px 10px 20px");
+        } else {
+            $(".weekend, .Szombat-display, .Vasárnap-display").css("display", "block");
+            $(".day_wrap, .weekdays div").css("margin", "10px");
+        }
+    });
+
 });
