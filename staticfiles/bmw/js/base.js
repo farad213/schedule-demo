8814fa199cb16306fd699dea5bf23f75d6a56898
farad213 <farad213@gmail.com>
2023-01-24 20:15:14 +0100

    mobile user button fix

--- staticfiles/bmw/js/base.js
+++ staticfiles/bmw/js/base.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $("#BMW-dropdown").click(function(){
       $("#dropdown_nav").toggle();
     });
+    $("#user-dropdown").click(function(){
+        $("#user-dropdown-content").toggle();
+    });
 });
